bec333a0e1fd1be2efd7c2a354ab5571d2eacc20
SAMMY <326589150+sqn10@users.noreply.github.com>
2026-09-21 12:40:54 -0400

    Add project files.

---
+++ introToMethods_3.5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace introToMethods_3._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
