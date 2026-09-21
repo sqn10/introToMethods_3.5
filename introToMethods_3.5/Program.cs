a8deebd8bb4e4f7ccf974304a960d19ace5dcb59
SAMMY <326589150+sqn10@users.noreply.github.com>
2026-09-21 13:06:35 -0400

    Might finish this one

--- introToMethods_3.5/Program.cs
+++ introToMethods_3.5/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
+            // may finish this??
             Console.WriteLine("Hello, World!");
+            DrawCheck();
+            DrawCheck();
+            DrawCheck();
+            DrawCheck();
+            Console.WriteLine();
+            DrawX();
+        }
+
+        public static void DrawCheck()
+        {
+            Console.WriteLine("     /");
+            Console.WriteLine("    /");
+            Console.WriteLine("\\  /");
+            Console.WriteLine(" \\/");
+        }
+
+        public static void DrawX()
+        {
+            Console.WriteLine("\\    /");
+            Console.WriteLine(" \\  /");
+            Console.WriteLine("  \\/");
+            Console.WriteLine("  /\\");
+            Console.WriteLine(" /  \\");
+            Console.WriteLine("/    \\");
         }
     }
 }
